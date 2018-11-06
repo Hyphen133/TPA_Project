f782215662f2d63dc3e5ff1aec89b3be482b2dc1
stanislaw-zakrzewski <stanislaw.zakrzewski13@gmail.com>
2018-11-06 10:38:09 +0100

    Started working on console app

--- ViewConsole/Program.cs
+++ ViewConsole/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TP.GraphicalData.TreeView;
+using TPAv2.Services;
+using ViewWPF.TreeView;
 
 namespace ViewConsole
 {
@@ -10,6 +13,16 @@ namespace ViewConsole
     {
         static void Main(string[] args)
         {
+            MyViewModel myViewModel = new MyViewModel();
+            myViewModel.PathVariable = "C:\\Users\\stz\\Google Drive\\Studia\\Informatyka\\5 Semestr\\Technologie programowania adaptacyjnego\\Projekt\\TPA_Project\\TPAv2\\TPA.ApplicationArchitecture.dll";
+            Console.WriteLine("oko");
+            myViewModel.Click_Browse.Execute(null);
+
+            Console.WriteLine(myViewModel.HierarchicalAreas.Count);
+            Console.WriteLine(myViewModel.HierarchicalAreas[0].Name);
+
+
+            Console.ReadKey();
         }
     }
 }
