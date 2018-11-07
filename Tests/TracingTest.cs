6775716568d58ecf139e6afb154e1c27518eb2c5
stanislaw-zakrzewski <stanislaw.zakrzewski13@gmail.com>
2018-11-07 21:17:24 +0100

    Started working on tracing test

--- Tests/TracingTest.cs
+++ Tests/TracingTest.cs
@@ -2,6 +2,9 @@
 using System.Text;
 using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using TPA.Reflection.Model;
+using TPAv2.Services;
 
 namespace Tests
 {
@@ -9,9 +12,24 @@ namespace Tests
     public class TracingTest
     {
         [TestMethod]
+        [ExpectedException(typeof(Exception))]
         public void TestMethod1()
         {
-            
+            string path = Directory.GetParent(Directory.GetCurrentDirectory()).ToString();
+            string root = "TPA_Project";
+            while (!(path.Substring(path.Length - root.Length) == root))
+            {
+                path = path.Remove(path.Length - 1);
+                Console.WriteLine(path);
+            }
+            Console.WriteLine(path);
+            string filename = "\\Tests\\TPA.ApplicationArchitecture.dll";
+            string xmlName = "\\Tests\\test.xml";
+            string fullFilePath = path + filename;
+            string fullXmlPath = path + xmlName;
+            Console.WriteLine(fullFilePath);
+            AssemblyMetadata assemblyMetaData = DataService.LoadAssembly(fullFilePath);
+            XmlSerialize xmlSerialize = new XmlSerialize(new FileTraceSource("hahaherroransko"));
         }
     }
 }
